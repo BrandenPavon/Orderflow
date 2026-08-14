94efbdfef531dace28e781c20781140a8678b377
bp <bp@bpsite.xyz>
2026-08-14 10:38:59 -0700

    feat: add market event side and type enums

--- include/market/event.hpp
+++ include/market/event.hpp
@@ -9,6 +9,11 @@ using Price = std::int64_t; // $10993.32 -> 1099332
 using Quantity = std::uint64_t; 
 using SymbolID = std::uint64_t; 
 
+enum class Side : std::uint8_t {
+    Buy  = 0, // Bid
+    Sell = 1  // Ask
+};
+
 class MarketEvent {
   Timestamp timestamp_ns;
   OrderID order_id;
