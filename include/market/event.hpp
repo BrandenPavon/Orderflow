dbb50b4addb18a64e20170b3954b410d9b25933d
bp <bp@bpsite.xyz>
2026-08-14 10:15:06 -0700

    feat: add MarketEvent

---
+++ include/market/event.hpp
@@ -0,0 +1,21 @@
+#ifndef EVENT_HPP
+#define EVENT_HPP
+
+#include <cstdint>
+
+using Timestamp = std::uint64_t; // Unix timestamp in nano seconds UTC
+using OrderID = std::uint64_t; // start at 1, 
+using Price = std::int64_t; // $10993.32 -> 1099332
+using Quantity = std::uint64_t; 
+using SymbolID = std::uint64_t; 
+
+class MarketEvent {
+  Timestamp timestamp_ns;
+  OrderID order_id;
+  Price price;
+
+  Quantity quantity;
+  SymbolID symbol_id;
+
+};
+#endif
